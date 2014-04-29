61c102e37a4f96a054c2395c396dc686d4095b51
John Goodall <jgoodall@gmail.com>
2014-04-29 15:03:30 -0400

    better error handling and cleanup

---
+++ README.md
@@ -0,0 +1,22 @@
+log-server
+==========
+
+[![Gobuild Download](http://gobuild.io/badge/github.com/jgoodall/log-server/download.png)](http://gobuild.io/github.com/jgoodall/log-server)
+
+Simple server for saving and retrieving JSON logs via  HTTP interface. 
+
+To download a binary, open a browser to [the gobuild page](http://gobuild.io/download/github.com/jgoodall/log-server).
+
+To start the server:
+
+    log-server -filepath="out.json" -logpath="out.log" -port=8000
+
+To save a log:
+
+    curl -i -XPOST http://localhost:8000/log -d '{"message": "test message one", "type": "curl", "tags": ["firsttag", "secondtag"]}' -H "content-type: application/json"
+
+    curl -i -XPOST http://localhost:8000/log -d '{"message": "test message two", "type": "curl", "tags": ["secondtag"]}' -H "content-type: application/json"
+
+To retrieve logs:
+
+    curl -i -XGET http://localhost:8000/logs -H "accept: application/json"
--- log-server.go
+++ log-server.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/ant0ine/go-json-rest/rest"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -15,6 +15,17 @@ import (
 
 // output file to save logs to
 var f *os.File
+var logger log.Logger
+
+// log message format
+// message is required, all other fields are optional
+// createdAt is automatically assigned
+type LogMessage struct {
+	Message   string    `json:"message"`
+	CreatedAt time.Time `json:"createdAt"`
+	Tags      []string  `json:"tags"`
+	Type      string    `json:"type"`
+}
 
 // response type for logs successfully saved
 type OkResp struct {
@@ -29,23 +40,41 @@ type ErrResp struct {
 	Error     string    `json:"error"`
 }
 
-// handler for POST requests to write logs
+// error handler - write to log and return error to client
+func Error(res rest.ResponseWriter, code int, err string) {
+	res.WriteHeader(code)
+	res.WriteJson(ErrResp{Ok: false, ErroredAt: time.Now(), Error: err})
+	logger.Println(err)
+}
+
+// handler for POST requests to write a log entry
+// returns a 200 if successfully saved, else an error code
 func PostLogHandler(res rest.ResponseWriter, req *rest.Request) {
 
-	content, err := ioutil.ReadAll(req.Body)
-	req.Body.Close()
+	logEntry := LogMessage{}
+	err := req.DecodeJsonPayload(&logEntry)
 	if err != nil {
-		e := "Error reading request body: " + err.Error()
-		res.WriteHeader(http.StatusInternalServerError)
-		res.WriteJson(ErrResp{Ok: false, ErroredAt: time.Now(), Error: e})
+		Error(res, http.StatusInternalServerError, "Error decoding request body: "+err.Error())
 		return
 	}
 
-	_, err = f.Write(content)
+	if logEntry.Message == "" {
+		Error(res, http.StatusInternalServerError, "Logs must include a message field")
+		return
+	}
+
+	logEntry.CreatedAt = time.Now()
+
+	js, err := json.Marshal(logEntry)
 	if err != nil {
-		e := "Error writing log message to file: " + err.Error()
-		res.WriteHeader(http.StatusInternalServerError)
-		res.WriteJson(ErrResp{Ok: false, ErroredAt: time.Now(), Error: e})
+		Error(res, http.StatusInternalServerError, "Error encoding request as JSON: "+err.Error())
+		return
+	}
+
+	_, err = f.Write(js)
+	_, err = f.Write([]byte("\n"))
+	if err != nil {
+		Error(res, http.StatusInternalServerError, "Error writing log message to file: "+err.Error())
 		return
 	}
 
@@ -53,14 +82,20 @@ func PostLogHandler(res rest.ResponseWriter, req *rest.Request) {
 	res.WriteJson(OkResp{Ok: true, CreatedAt: time.Now()})
 }
 
-// handler for POST requests to write logs
+// handler for GET requests to retrieve logs
+// returns the newline delimited logs
 func GetLogsHandler(res rest.ResponseWriter, req *rest.Request) {
 	res.WriteHeader(http.StatusOK)
 	r := bufio.NewReader(f)
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		res.WriteJson(scanner.Text())
+		line := scanner.Bytes()
+		res.(http.ResponseWriter).Write(line)
 		res.(http.ResponseWriter).Write([]byte("\n"))
+	}
+	if scanner.Err() != nil {
+		Error(res, http.StatusInternalServerError, "Error reading output file: "+scanner.Err().Error())
+		return
 	}
 	res.(http.Flusher).Flush()
 }
